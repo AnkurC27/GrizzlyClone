2d42745bb50097c19a5c9ad6a6c564fbe129560d
Ankur Chadha <ankur.chadha@bnbuilders.com>
2023-08-25 14:02:26 -0700

    filename change

--- battery.py
+++ battery.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse
 from selenium.common.exceptions import TimeoutException
 
 EXCEL_FILE_PATH = r'C:\Users\ankur.chadha\Desktop\GrizzlyProject\excel\RatesMaster2024.V2.xlsx'
-SHEET_NAME = "2024 Rate Links"
+SHEET_NAME = "2024 Rate Filter"
 EDGE_DRIVER_PATH = r'C:\Users\ankur.chadha\desktop\msedgedriver'
 ADDED_ITEMS_DIR = "AddOns"
 
